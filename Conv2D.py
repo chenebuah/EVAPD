0791fcec0d908c98e96cfcab52c9f8765dd267f4
Ericsson Chenebuah <74286898+chenebuah@users.noreply.github.com>
2023-05-30 17:12:29 -0400

    Conv2D.py
    Two-dimensional Convolutional Neural Networks (Conv2D) for predicting and classifying deterministic targets used to update the fitness function of the genetic algorithm (GA-Similarity.py).

---
+++ Conv2D.py
@@ -0,0 +1,34 @@
+# Convolutional Neural Netowrks for feedbacking 'GA-Similarity.py' fitness functions.
+# The network architectures are the same for predicting energy above convex hull (Ehull) values and classifying ICSD labels. 
+# However, modifications must be made at the output (final) dense layer and '.compile' to ensure the activation and loss functions correspond to the actual prediction/classification process. 
+
+import keras
+from keras import layers
+from tensorflow.keras.optimizers import Adam
+
+input = keras.Input(shape=(X_samples.shape[1], X_samples.shape[2], X_samples.shape[3]))
+
+x = layers.Conv2D(8, (3, 3), activation='relu', strides= 1, padding='same')(input)
+x = layers.Conv2D(16, (3, 3), activation='relu', strides=2, padding='same')(x)
+x = layers.Conv2D(32, (3, 3), activation='relu', strides=2, padding='same')(x)
+
+x = layers.MaxPooling2D((2, 2), padding='same')(x)
+x = layers.Dropout(0.2)(x)
+
+x = layers.Flatten()(x)
+
+x = layers.Dense(256, activation='relu')(x)
+x = layers.Dense(64, activation='relu')(x)
+x = layers.Dense(16, activation='relu')(x)
+x = layers.Dense(4, activation='relu')(x)
+
+target = layers.Dense(1, activation='linear')(x) # If predicting continuous Ehull values
+#target = layers.Dense(1, activation='sigmoid')(x) # If classifying ICSD labels  
+    
+opt = Adam(learning_rate=1e-3, decay=1e-3/200)
+    
+Ehull_model = keras.Model(input, target)
+#ICSD_model = keras.Model(input, target)
+
+Ehull_model.compile(loss="mean_squared_error", optimizer=opt, metrics=['mae'])
+#ICSD_model.compile(loss="binary_cross_entropy", optimizer=opt, metrics=['accuracy'])
